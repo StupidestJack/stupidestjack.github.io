3f3a97e15eb48913158ba92a739480f3993442e1
Niugnep <niugnep87@proton.me>
2026-05-10 11:30:06 +0800

    MOTHERFUCK

---
+++ scripts/motherfuck.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const container = document.getElementById('posts-container');
+
+    fetch("posts.json")
+    .then(res => res.json())
+    .then(posts => {
+
+        container.innerHTML = "";
+
+        // Object.entries 轉陣列
+        const postsArray = Object.entries(posts);
+
+        // 最新文章排前面
+        postsArray.reverse();
+
+        postsArray.forEach(([postName, data]) => {
+
+            const article = document.createElement('article');
+
+            article.className = "post-card";
+
+            article.innerHTML = `
+                <h3>${data.title}</h3>
+
+                <p class="post-time">
+                    ${data.time[0]}/${data.time[1]}/${data.time[2]}
+                </p>
+
+                <p>
+                    ${data.description}
+                </p>
+                <div class="tags">
+                    ${
+                        data.tags
+                        ? data.tags.map(tag =>
+                            `<span class="tag">${tag}</span>`
+                        ).join("")
+                        : ""
+                    }
+                </div>
+            `;
+            article.style.cursor = "pointer";
+            article.addEventListener('click', () => {
+                window.location.href =
+                    `post_motherfuck.html?post=${postName}`;
+            });
+
+            container.appendChild(article);
+        });
+
+    });
+});
