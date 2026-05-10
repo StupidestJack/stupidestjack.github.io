a65211a32bd817c731088f54546b56c99fd8a4cf
Niugnep <niugnep87@proton.me>
2026-05-10 11:35:26 +0800

    媽 的 發

--- scripts/headtail.js
+++ scripts/headtail.js
@@ -4,8 +4,8 @@ const headerHTML = `
 <div class="logo"><a href="index.html">Niugnep 的部落格</a></div>
 <nav>
 <a href="index.html">首頁</a>
-<a href="about.html">關於我</a>
-<a href="posts.html">所有文章</a>
+<a href="post.html?post=self-include">關於我</a>
+<a href="motherfuck.html">媽的發</a>
 </nav>
 </header>
 `;
