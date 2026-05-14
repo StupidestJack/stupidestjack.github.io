9fc54bf3dd0d6e115a2aa7f8facf91bf1b1006fd
Niugnep <niugnep87@proton.me>
2026-05-14 20:56:23 +0800

    加<!DOCTYPE html>跟圖示不用全部重尻就是爽

--- buildpy/htmlmaker.py
+++ buildpy/htmlmaker.py
@@ -1,7 +1,8 @@
-html_template = """<html lang="zh-Hant"><head>
+html_template = """<!DOCTYPE html><html lang="zh-Hant"><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{}</title>
     {}
+    <link rel="icon" href="https://avatars.githubusercontent.com/u/229949534?v=4&size=64" type="image/png">
     <link rel="stylesheet" href="/blog/sty.css">
 </head><body>{}</body></html>"""
