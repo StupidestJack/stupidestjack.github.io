9f19ab5238deb737e8adc915020e228bcd557dbc
Niugnep <niugnep87@proton.me>
2026-07-09 14:12:53 +0800

    end: 這個倉庫正式畢業！

--- buildpy/metadata.py
+++
@@ -1,9 +0,0 @@
-import config as conf
-
-desc = '<meta name="description" content="{}">'
-og_title = '<meta property="og:title" content="{}">'
-og_desc = '<meta property="og:description" content="{}">'
-
-author = f'<meta name="author" content="{conf.author}">'
-canonical = f'<link rel="canonical" href="{conf.url}/posts/{{}}.html">'
-pub_date = '<meta property="article:published_time" content="{}">'
